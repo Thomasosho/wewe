230f7653199b907c3cc6dbd3eaac4e27cbf0ee59
Thomasosho <tomasosho@gmail.com>
2022-01-27 18:53:41 +0100

    love you

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -19,7 +19,8 @@ class AuthController extends Controller
         $field = $request->validate([
             'name'    =>  'required|string',
             'email' =>  'required|string|unique:users,email',
-            'password'  =>  'required|string|confirmed'
+            'password'  =>  'required|string|confirmed',
+            'user_type' => 'nullable'
         ]);
 
         $user = User::create([
